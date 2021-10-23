0507c9737b1c80eda201a2c0a3b7f37267210b26
Ori5000 <43366498+Ori5000@users.noreply.github.com>
2021-10-23 18:18:21 +0300

    Update webdl.py

--- webdl.py
+++ webdl.py
@@ -16,17 +16,9 @@ arguments = argparse.ArgumentParser()
 # arguments.add_argument("-m", "--video-link", dest="mpd", help="MPD url")
 arguments.add_argument("-o", '--output', dest="output", help="Specify output file name with no extension", required=True)
 arguments.add_argument("-id", dest="id", action='store_true', help="use if you want to manually enter video and audio id.")
-arguments.add_argument("-s", dest="subtitle", help="enter subtitle url")
-arguments.add_argument("-k", dest="keyfile", action='store_true', help="Use keyfile with the same name as specified output")
-arguments.add_argument("-d", dest="delenc", action='store_true', help="Delete encoded AND JSON FILE upon completion")
 args = arguments.parse_args()
 
-if args.keyfile:
-    keyfile = str(args.output) + ".json"
-else:
-    keyfile = "keys.json"
-
-with open(keyfile) as json_data:
+with open("keys.json") as json_data:
     config = json.load(json_data)
     json_mpd_url = config[0]['mpd_url']
     try:
@@ -43,12 +35,11 @@ realPath = os.path.realpath(currentFile)
 dirPath = os.path.dirname(realPath)
 dirName = os.path.basename(dirPath)
 
+mkvmerge = './mkvmerge'
 mp4decrypt = './Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4decrypt'
-SubtitleEditexe = dirPath + '/binaries/SubtitleEdit.exe'
 
 # mpdurl = str(args.mpd)
 output = str(args.output)
-subtitle = str(args.subtitle)
 
 if args.id:
     print(f'Selected MPD : {json_mpd_url}\n')    
@@ -66,28 +57,15 @@ else:
 
 
 print("\nDecrypting .....")
-subprocess.run([mp4decrypt, '--show-progress', keys, 'encrypted.m4a', 'decrypted.m4a'])
-subprocess.run([mp4decrypt, '--show-progress', keys, 'encrypted.mp4', 'decrypted.mp4'])
+subprocess.run(f'{mp4decrypt} --show-progress {keys} encrypted.m4a decrypted.m4a', shell=True)
+subprocess.run(f'{mp4decrypt} --show-progress {keys} encrypted.mp4 decrypted.mp4', shell=True)  
 
-if args.subtitle:
-    subprocess.run(f'{aria2cexe} {subtitle}', shell=True)
-    os.system('ren *.xml en.xml')
-    subprocess.run(f'{SubtitleEditexe} /convert en.xml srt', shell=True) 
-    print("Merging .....")
-    subprocess.run(['./mkvmerge', '--ui-language' ,'en', '--output', output +'.mkv', '--language', '0:eng', '--default-track', '0:yes', '--compression', '0:none', 'decrypted.mp4', '--language', '0:eng', '--default-track', '0:yes', '--compression' ,'0:none', 'decrypted.m4a','--language', '0:eng','--track-order', '0:0,1:0,2:0,3:0,4:0', 'en.srt'])
-    print("\nAll Done .....")
-else:
-    print("Merging .....")
-    subprocess.run(['./mkvmerge', '--ui-language' ,'en', '--output', output +'.mkv', '--language', '0:eng', '--default-track', '0:yes', '--compression', '0:none', 'decrypted.mp4', '--language', '0:eng', '--default-track', '0:yes', '--compression' ,'0:none', 'decrypted.m4a','--language', '0:eng','--track-order', '0:0,1:0,2:0,3:0,4:0'])
-    print("\nAll Done .....")    
+print("Merging .....")
+subprocess.run([mkvmerge, '--ui-language' ,'en_US', '--output', output +'.mkv', '--language', '0:eng', '--default-track', '0:yes', '--compression', '0:none', 'decrypted.mp4', '--language', '0:eng', '--default-track', '0:yes', '--compression' ,'0:none', 'decrypted.m4a','--language', '0:eng'])
+print("\nAll Done .....")    
 
-if args.delenc:
-    delete_choice = 1
-    if os.path.isfile(output + ".mkv"):
-        os.remove(keyfile)
-else:
-    print("\nDo you want to delete the Encrypted Files : Press 1 for yes , 2 for no")
-    delete_choice = int(input("Enter Response : "))
+print("\nDo you want to delete the Encrypted Files : Press 1 for yes , 2 for no")
+delete_choice = int(input("Enter Response : "))
 
 if delete_choice == 1:
     os.remove("encrypted.m4a")
